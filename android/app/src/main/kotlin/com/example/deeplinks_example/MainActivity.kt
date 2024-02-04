75609a890be82cfd0981ca179c87b9b6fa0959a1
sharkFlutterDev <129946961+sharkFlutterDev@users.noreply.github.com>
2024-02-04 19:18:25 +0200

    First commit and added project

---
+++ android/app/src/main/kotlin/com/example/deeplinks_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deeplinks_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  runApp(MaterialApp.router(routerConfig: router));
+}
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(title: const Text('Home Screen')),
+      ),
+      routes: [
+        GoRoute(
+          path: 'details',
+          builder: (_, __) => Scaffold(
+            appBar: AppBar(
+              title: const Text('Details Screen'),
+            ),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
